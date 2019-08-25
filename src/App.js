c2206dacfd2a2718fe8cf3e9802e4840950e7539
James Gary <gmarchesi888@gmail.com>
2019-08-25 14:48:53 -0400

    Working at last.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,44 @@
 import React, { Component } from "react";
-import cx from "classnames";
 import { CSSTransition } from "react-transition-group";
 import "./App.css";
 
-
 class App extends Component {
   state = {
-    showList: true,
-    highlightedHobby: false
+    showList: false
   };
 
-  switch = () => {
+  toggle = () => {
     this.setState(prevState => ({
       showList: !prevState.showList
-    }));
-  };
-
-  listSwitch = () => {
-    this.setState(state => ({
-      highlightedHobby: !state.highlightedHobby
     }));
   };
 
   render() {
     return (
       <div className="container-outer">
-        <button className="display" onClick={this.switch}>
-          Show List
-        </button>
+        {this.state.showList ? (
+          <div className="spacer5" />
+        ) : (
+          <button className="display" onClick={this.toggle}>
+            Toggle Element
+          </button>
+        )}
         <CSSTransition
           in={this.state.showList}
           timeout={400}
-          classNames="list-transition"
+          classNames="fade"
           unmountOnExit
           appear
-          onEntered={this.listSwitch}
-          onExit={this.listSwitch}
+          onEntered={this.switch}
+          onExit={this.switch}
         >
           <div className="container-inner">
-            <h3>I Appear and Disappear</h3>
+            <h1>I Appear and Disappear</h1>
+            <div className="spacer10" />
+            <button className="display" onClick={this.toggle}>
+              Toggle Element
+            </button>
+            <div className="spacer10" />
           </div>
         </CSSTransition>
       </div>
