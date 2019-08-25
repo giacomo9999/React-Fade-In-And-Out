52c596b7d86de93fd1635ed06225c2128715ab7b
James Gary <gmarchesi888@gmail.com>
2019-08-25 14:17:30 -0400

    Rudimentary cut-and-pasting...but at least it works.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import cx from "classnames";
+import { CSSTransition } from "react-transition-group";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+class App extends Component {
+  state = {
+    showList: true,
+    highlightedHobby: false
+  };
+
+  switch = () => {
+    this.setState(prevState => ({
+      showList: !prevState.showList
+    }));
+  };
+
+  listSwitch = () => {
+    this.setState(state => ({
+      highlightedHobby: !state.highlightedHobby
+    }));
+  };
+
+  render() {
+    return (
+      <div className="container-outer">
+        <button className="display" onClick={this.switch}>
+          Show List
+        </button>
+        <CSSTransition
+          in={this.state.showList}
+          timeout={400}
+          classNames="list-transition"
+          unmountOnExit
+          appear
+          onEntered={this.listSwitch}
+          onExit={this.listSwitch}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <div className="container-inner">
+            <h3>I Appear and Disappear</h3>
+          </div>
+        </CSSTransition>
+      </div>
+    );
+  }
 }
 
 export default App;
